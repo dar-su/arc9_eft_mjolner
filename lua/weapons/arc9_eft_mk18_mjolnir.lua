0b061837bf851c340b349b73b7c7d312f08fd712
Darsu <95997959+dar-su@users.noreply.github.com>
2025-05-07 14:14:28 +0700

    tpik angle fix

--- lua/weapons/arc9_eft_mk18_mjolnir.lua
+++ lua/weapons/arc9_eft_mk18_mjolnir.lua
@@ -34,7 +34,7 @@ SWEP.WorldModelOffset = {
     Pos = Vector(-8.3, 5.5, -6),
     Ang = Angle(-7, 0, 180),
     TPIKPos = Vector(-4, 3, -2), -- rpg
-    TPIKAng = Angle(-5, 0, 180),
+    TPIKAng = Angle(-11.5, 0, 180),
     Scale = 1
 }
 
